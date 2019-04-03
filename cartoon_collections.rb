f482d4c67fb00f80e7c17b1618513af9e43ba1fb
James Pier <jpier2012@gmail.com>
2019-04-03 19:02:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,4 +15,4 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-roll_call_dwarves %w(Dopey Sleepy Bashful)
+summon_captain_planet %w(dopey sleepy bashful)
