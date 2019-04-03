18b09e59b5cef577f2e2b3f4883e429bfd992a48
James Pier <jpier2012@gmail.com>
2019-04-03 19:37:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_list = array & cheese_types
-  cheese_list.first
+  return cheese_list.first
 end
 
 puts find_the_cheese []
