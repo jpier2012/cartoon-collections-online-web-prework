7be423165c17dd4a90aba99f64ed7d16febc687f
James Pier <jpier2012@gmail.com>
2019-04-03 19:25:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet elements
 end
 
 def long_planeteer_calls array
-  if array.any? {|call| call.length >= 4}
+  if array.any? {|call| call.length > 4}
     true
   else
     false
