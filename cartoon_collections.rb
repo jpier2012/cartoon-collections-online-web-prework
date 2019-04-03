92f33e167bffebe4067349f8ebedcea38036079e
James Pier <jpier2012@gmail.com>
2019-04-03 19:35:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls array
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_list = cheese_types & array
+  cheese_list.first unless nil
 end
